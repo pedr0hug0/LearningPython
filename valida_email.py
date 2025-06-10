309a26ab7482a39195e626acf368b7433be75d78
Pedro França <pedro.franca@ifsp.edu.br>
2025-06-10 10:58:04 -0300

    Fix valida email with orders args

--- valida_email.py
+++ valida_email.py
@@ -1,8 +1,12 @@
 import sys
 # Entrada do usuário
 email = input().strip()
-#verificar se é um email válido
-if '@' in email and '.' in email.split('@')[-1]:
-    print("Email válido")
+#verificar se é um email válido  
+# Regras para um e-mail válido:
+#Deve conter o caractere "@" e um domínio, como "gmail.com" ou "outlook.com".
+#Não pode começar ou terminar com "@".
+#Não pode conter espaços.
+if "@" in email and "." in email.split("@")[-1] and not email.startswith("@") and not email.endswith("@") and " " not in email:
+    print("E-mail válido")
 else:
-    print("Email inválido")
+    print("E-mail inválido")
